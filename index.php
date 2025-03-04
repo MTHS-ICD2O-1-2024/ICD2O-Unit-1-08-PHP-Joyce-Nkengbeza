48960e2778f2038d16d5faf422fa18c0f58116c7
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-04 17:58:09 +0000

    added image

--- index.php
+++ index.php
@@ -39,20 +39,17 @@
 
 
   <body>
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
-  <script>
-    src = "./js/script.js"
-  </script>
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-    <div class="mdl-layout__header-row">
-      <span class="mdl-layout-title">The XXX program, with PHP</span>
-      </div>
-    </header>
-    <main class="mdl-layout__content">
-    <div class="page-content">Intructions here ...</div>
-    <img src="./images/cinnamoroll.jpeg" />
-</div>
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+    <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+      <header class="mdl-layout__header">
+        <div class="mdl-layout__header-row">
+          <span class="mdl-layout-title">The XXX program, PHP</span>
+        </div>
+      </header>
+      <main class="mdl-layout__content">
+        <div class="right-image">
+          <img src="./images/cinnamoroll,jpeg" alt="Cinnamoroll" />
+        </div>
         <div class="page-content">Instructions here ...</div>
         <br />
         <!-- Simple Textfield for integers-->
