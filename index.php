2acda6b4a83c1c0f57089968cf4993fb3e450a35
default <Joyce.nkengbeza@stu.ocsb.ca>
2025-03-04 18:11:32 +0000

    error

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
       </header>
       <main class="mdl-layout__content">
         <div class="right-image">
-          <img src="./images/cinnamoroll,jpeg" alt="Cinnamoroll" />
+          <img src="./images/cinnamoroll.jpeg" alt="Cinnamoroll" />
         </div>
         <div class="page-content">Instructions here ...</div>
         <br />
